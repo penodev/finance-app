d2f37b4b4f8831f21319982079ec602935a52b2e
tp <60010482@kmitl.ac.th>
2024-05-12 16:14:19 +0700

    feat: authentication

---
+++ middleware.ts
@@ -0,0 +1,14 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { protectedRoute } from "./constants/route";
+
+const isProtectedRoute = createRouteMatcher(["/"]);
+
+export default clerkMiddleware((auth, request) => {
+  if (isProtectedRoute(request)) {
+    auth().protect();
+  }
+});
+
+export const config = {
+  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
