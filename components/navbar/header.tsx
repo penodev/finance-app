8739b9d86eb989e400002404a93dd059f49693b3
tp <103176224+penodev@users.noreply.github.com>
2024-07-20 00:10:39 +0700

    feat: date & account filters

--- components/navbar/header.tsx
+++ components/navbar/header.tsx
@@ -1,6 +1,8 @@
 import { ClerkLoaded, ClerkLoading, UserButton } from "@clerk/nextjs";
 import { Loader2 } from "lucide-react";
 
+import { Filters } from "@/components/navbar/filters";
+
 import { HeaderLogo } from "./header-logo";
 import { Navigation } from "./navigation";
 import { WelcomeMsg } from "../welcome-msg";
@@ -22,6 +24,7 @@ export const Header = () => {
           </ClerkLoading>
         </div>
         <WelcomeMsg />
+        <Filters />
       </div>
     </div>
   );
